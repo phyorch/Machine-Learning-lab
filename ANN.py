3037b362b6e998ba9aab699d9dccca67c8611149
Phyorch <405918147@qq.com>
2017-11-13 23:55:05 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lab1.py

--- ANN.py
+++ ANN.py
@@ -1,8 +1,6 @@
 # to construct a aritificial neural network
-import csv
-import math
 import numpy as np
-import scipy as sp
+import pandas as pd
 
 #main paramenters
 #node_matrix   list[array]  containing every layer node value  first element in is input last is output
@@ -10,95 +8,66 @@ import scipy as sp
 #dataset_x
 #dataset_y     2D array
 
+# Each of the image contains 28*28=784 pixels
+# labels is the y value list of each output so it should be turned to a matrix or 2-D array
+# to construct a aritificial neural network
+
+# main paramenters
+# node_matrix list[array]containing every layer node valuefirst element in is input last is output
+# Theta list[arrat]containing every layer theta parameters as a 2D array
+# dataset_x
+# dataset_y 2D array
 
 # Each of the image contains 28*28=784 pixels
 # labels is the y value list of each output so it should be turned to a matrix or 2-D array
-def data_read(adress):
-    data_train = csv.reader(
-        open(adress))
-    dataset_x = []
-    labels = []
-
-    for row in data_train:
-        labels.append(row.pop(0))
-        dataset_x.append(row)
-    del dataset_x[0]
-    del labels[0]
-    dataset_x = [[int(elem) for elem in row] for row in dataset_x]
-    labels = [int(elem) for elem in labels]
-    dataset_x = np.array(dataset_x)
-    dataset_y = np.zeros((len(dataset_x),10))
-    for i in range(len(dataset_y)):
-        pos = labels[i]
-        dataset_y[i,pos] = 1
-    return dataset_x, dataset_y, labels
-
-
 
 # contains the unit amount of each hidden layer
-# first eloement is input and last is output
+# first element is input and last is output
 # Theta contains the randomly initialized theta matrix from current layer to next
 class Layer(object):
     def __init__(self, layer_order, layer_size):
         self.size = layer_size
         self.order = layer_order
+        self.node = np.random.randn(self.size, 1)
+        self.error = np.random.randn(self.size, 1)
+        self.bias = np.random.randn(self.size, 1)
     def Theta_init(self, next_layer_size):
-        epsilon = 0.5
-        self.theta = np.random.rand(next_layer_size, self.size)
-        self.theta = self.theta * 2 * epsilon - epsilon
-        self.bias = np.random.rand(1, next_layer_size)
-    def Node_init(self):
-        self.node = np.random.rand(1, self.size)
-    def Error_init(self):
-        self.error = np.random.rand(1, self.size)
-
+        self.theta = np.random.randn(next_layer_size, self.size)
 
 def activation(x):
-    x = 1/(1 + np.exp(-x))
+    x = 1 / (1 + np.exp(-x))
     return x
 
-def FP_process(Network, input):  # input is array
+def FP_process(Network, input):# input is array
     for l in range(len(Network)):
         layer = Network[l]
-        if layer.order==0: # if we are calculating the input layer to seconde layer
-            #node_matrix.append(input)
+        if layer.order == 0:  # if we are calculating the input layer to seconde layer
+            # node_matrix.append(input)
             layer.node = input
-        else: #  we use previous node array to calculate the new one
-            prior = Network[l-1]
-            prior_node = prior.node
-            prior_theta = prior.theta
-            b = np.dot(prior.theta, prior_node.T)
-            c = prior.bias
-            layer.node = activation(b.T + c)
+        else:  # we use previous node array to calculate the new one
+            layer = Network[l]
+            prior = Network[l - 1]
+            layer.node = activation(np.dot(prior.theta, prior.node) + layer.bias)
     return Network
 
 
-
-def BP_process(Network, y): # output is one of the dataset's y vector
-    for l in range(len(Network)-1,-1,-1):
-        if l==len(Network)-1:
-            n = Network[l].node
-            Network[l].error = (Network[l].node - y).T * Network[l].node * (1-Network[l].node)
-            q = Network[l].error
+def BP_process(Network, y):# output is one of the dataset's y vector
+    for l in range(len(Network)-1, -1, -1):
+        if l == len(Network) - 1:
+            layer = Network[l]
+            layer.error = (layer.node - y) * layer.node * (1 - layer.node)
         else:
             layer = Network[l]
-            post_layer = Network[l+1]
-            e = post_layer.error
-            d = np.dot(layer.theta.T, post_layer.error.T)
-            layer.error = np.dot(layer.theta.T, post_layer.error.T).T * layer.node * (1-layer.node) # back propagation
+            post_layer = Network[l + 1]
+            layer.error = np.dot(layer.theta.T, post_layer.error) * layer.node * (1 - layer.node)  # back propagation
     return Network
 
-
-def Network_inital(X, Y, size, layer_num=3):
-    M = len(Y)
+def Network_initail(size, layer_num=3):
     Network = []
-    Error_total = []
     for i in range(layer_num):
         layer = Layer(i, size[i])
-        layer.Node_init()
-        layer.Error_init()
-        if i<layer_num-1:
-            layer.Theta_init(size[i+1])
+        if i < layer_num - 1:
+            layer.Theta_init(size[i + 1])
         Network.append(layer)
     return Network
 
@@ -108,44 +77,58 @@ def Training(Network, X, Y, learning_rate=0.2):
     for m in range(M):
         x = X[m]
         y = Y[m]
+        x.shape = (x.shape[0],1)
+        y.shape = (y.shape[0],1)
         Network = FP_process(Network, x)
         Network = BP_process(Network, y)
-        for l in range(len(Network)-1):
+        for l in range(len(Network) - 1):
             ll = l + 1
-            Network[l].node.shape = (1,Network[l].size)
-            Network[l].theta -= learning_rate * np.dot(Network[ll].error.T, Network[l].node)
-            Network[l].bias -= learning_rate * Network[ll].error
+            layer = Network[l]
+            post_layer = Network[ll]
+            # Network[l].node.shape = (1,Network[l].size)
+            layer.theta -= learning_rate * np.dot(post_layer.error, layer.node.T)
+            post_layer.bias -= learning_rate * post_layer.error
     return Network
 
 def Test(Network, X, Y):
-    values = []
+    predict = []
+    right = 0
     for i in range(X.shape[0]):
-        Network = FP_process(Network, X[i])
-        result = Network[-1].node
-        values.append(result)
-        #for elem in range(len(result)):
-        #    if result[elem]==1:
-        #        values.append(elem)
-        #        break
-    return values
+        x = X[i]
+        x.shape = (x.shape[0], 1)
+        Network = FP_process(Network, x)
+        evaluate = Network[-1].node
+        evaluate.shape = (1, evaluate.shape[0])
+        evaluate = evaluate[0]
+        evaluate = evaluate.tolist()
+        values = evaluate.index(max(evaluate))
+        predict.append(values)
+        if values==Y[i]:
+            right += 1
+    accuracy = right/len(Y)
+    return right, accuracy, predict
 
 
-data_X, data_Y, a = data_read('C:/Users/Phyorch/Desktop/Learning/Mchine learning/project and homework/lab3/train.csv')
-test_X, test_Y, labels = data_read('C:/Users/Phyorch/Desktop/Learning/Mchine learning/project and homework/lab3/test.csv')
-X = data_X[:39000,:]
-Y = data_Y[:39000,:]
-X = X/255
-Y = Y/255
+def code(Y):
+    y_network = np.zeros((len(Y), 10))
+    for i in range(len(Y)):
+        y_network[i, Y[i]] = 1
+    return y_network
+
+data = pd.read_csv('C:/Users/Phyorch/Desktop/Learning/Mchine learning/project and homework/lab3/train.csv')
+data.as_matrix
+data = np.array(data)
+labels = data[:, 0]
+labels_network = code(labels)
+data = data[:, 1:]
+X = data[:39000, :]/255
+Y = labels_network[:39000, :]/255
 sizelist = [784, 30, 10]
-Network = Network_inital(X, Y, sizelist)
+test_X = data[:39000, :]/255
+test_Y = labels[:39000] /255
+
+Network = Network_initail(sizelist)
 Network = Training(Network, X, Y)
-test_X = data_X[39999:41000,:]
-test_Y = data_Y[39999:41000,:]
-answer = []
-for i in range(len(test_X)):
-    network = FP_process(Network, test_X[i])
-    answer.append(network[-1].node)
-answer = np.array(answer)
-values = np.array(Test(Network, test_X, test_Y))
-answer = labels[39999:41000]
-b = 1
+right, accuracy, predict = Test(Network, test_X, test_Y)
+a = Network[2].node
+
